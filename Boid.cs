3fea27c676ce4b5737126cc929d14da0cfd1a21a
cristianofederici02 <74297429+cristianofederici02@users.noreply.github.com>
2024-05-21 15:19:41 +0200

    Add files via upload

---
+++ Boid.cs
@@ -0,0 +1,93 @@
+﻿using Aiv.Fast2D;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOIDS
+{
+    internal class Boid
+    {
+        public Vector2 Position { get { return sprite.position; } set { sprite.position = value; } }
+
+        public Vector2 Velocity;
+
+        public float speed = 200f;
+
+        Sprite sprite;
+
+        public static List<Boid> Boids;
+
+        public static int BoidAmount;
+
+        public static Boid []Vboids;            
+        public Boid(Vector2 position, Vector2 velocity)
+        {
+            sprite = new Sprite(Program.Texture.Width, Program.Texture.Height);
+
+            sprite.pivot = new Vector2(Program.HalfWith,Program.HalfHeight);
+
+            Boids = new List<Boid>();
+
+            Position = position;
+            Velocity = velocity;
+        }
+
+        public void Update ()
+        {
+            Position += (Velocity * speed * Program.DeltaTime);
+        }
+
+        public void Draw()
+        {
+            sprite.DrawTexture(Program.Texture);
+        }
+
+        public void WrapAround(Boid boid)
+        {
+            if (boid.Position.X < 0)
+                boid.sprite.position.X += Program.Window.Width;
+            if (boid.Position.X > Program.Window.Width)
+                boid.sprite.position.X -= Program.Window.Width;
+            if (boid.Position.Y < 0)
+                boid.sprite.position.Y += Program.Window.Height;
+            if (boid.Position.Y > Program.Window.Height)
+                boid.sprite.position.Y -= Program.Window.Height;
+        }
+
+        public EventHandler ClickEvent;
+        public void OnClick()
+        {
+            ClickEvent.Invoke(this, EventArgs.Empty);
+        }
+        public void SpawnBoid(Vector2 position, Vector2 velocity)
+        {
+            Boid newBoid = new Boid(position, velocity);
+            Boids.Add(newBoid);
+        }
+
+        public void Init() //generates X balls, out of which 1 red 
+        {
+            //spawn balls int totBoid
+            //BoidAmount = totBoid;
+            //Vboids = new Boid[BoidAmount];
+            //for (int i = 0; i < Vboids.Length; i++)
+            //{
+            
+            //if (Program.Window.MouseLeft)
+            //    {
+            //        Console.WriteLine("mouse clik");
+            //        Boid boid = new Boid(new Vector2(Program.Window.Width * 0.5f, Program.Window.Height * 0.5f), new Vector2(1,0));
+            //        sprite = new Sprite(Program.HalfWith, Program.HalfHeight);
+            //        Boids.Add(boid);
+            //    }
+                
+            //}
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using Aiv.Fast2D;
+using OpenTK;
+using OpenTK.Graphics.ES11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOIDS
+{
+    internal class Program
+    {
+        public static Texture Texture { get; private set; }
+        public static float HalfWith { get { return Texture.Width * 0.5f; } }
+        public static float HalfHeight { get { return Texture.Height * 0.5f; } }
+        public static Window Window { get; private set; }
+        public static float DeltaTime { get { return Window.DeltaTime; } }
+
+        
+
+        public Vector2 mousePosition;
+        static void Main(string[] args)
+        {
+            Window = new Window(1600, 800 ,"Boids");
+
+            
+
+            Texture = new Texture("Assets/boid.png");
+
+            //Boid newBoid = new Boid();
+
+            Boid b;//= new Boid(Window.MousePosition, new Vector2(1, 0));
+
+             
+
+            while (Window.IsOpened)
+            {
+
+
+                
+                if (Window.MouseLeft)
+                {
+
+                    //Boid newBoid.SpawnBoid(Window.MousePosition, new Vector2(1, 0));
+                    b.Position = Window.MousePosition;
+                }
+
+
+                //b.SpawnBoid();
+                //b.Init();
+                //b = OnMouseLeftClick();
+                b.SpawnBoid(Window.MousePosition, new Vector2(1, 0));
+                b.Update();
+                b.Draw();
+                b.WrapAround(b);
+                
+
+                Window.Update();
+            }
+        }
+
+        public void OnMouseLeftClick()
+        {
+           // Boid.SpawnBoid(mousePosition);
+            //if (Window.MouseLeft)
+            //{
+            //    Boid newBoid = new Boid(Window.MousePosition);
+            //    Boids.Add(newBoid);  
+            //}
+        }
+
+    }
+}
